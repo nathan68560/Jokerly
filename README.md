780a1c8c9d36b1c3c38563613198647e68060a2b
Nathan <nathan.wersinger.1@ens.etsmtl.ca>
2024-05-15 16:50:11 +0200

    Added routing packages and started the homepage widget

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Jokerly 🃏
-~*_Become jokerly good at anything with flashcards!_*~
+\~*_Become jokerly good at anything with flashcards!_*\~
 
 Jokerly is yet another flashcard application that help you learn and memorize anything you want. The application is developed with Flutter for the web platform, thus letting you use it "au bureau"🧑‍💻 and on the go📱!
 
---
+++ lib/elements.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Deck extends StatelessWidget {
+  const Deck({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+class Flashcard extends StatefulWidget {
+  const Flashcard({super.key});
+
+  @override
+  State<Flashcard> createState() => _FlashcardState();
+}
+
+class _FlashcardState extends State<Flashcard> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
